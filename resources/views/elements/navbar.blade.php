ecdc36af8510cb567f9dd8456f5cc2db1c6fce5a
Adrian Crisan <adrian.crisan88@gmail.com>
2016-11-07 07:38:21 +0000

    Navbar brand link bugfix

--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav>
         <div class="nav-wrapper">
             <ul class="left hide-on-med-and-down">
-                <li><a href="/" class="waves-effect"><i class="material-icons">home</i></a></li>
+                <li><a href="{{ url('/') }}" class="waves-effect"><i class="material-icons">home</i></a></li>
                 <li>
                     <a href="{{ action('TasksController@index') }}" class="waves-effect"><i class="material-icons right">content_paste</i> Tasks</a>
                 </li>
