9e779f456a296c04b478e52e7a056f4a2b3036a4
crlcu <adrian.crisan88@gmail.com>
2017-01-26 16:41:18 +0200

    Email layout: remove copyright

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -592,41 +592,6 @@
     </tbody>
 </table></td>
                             </tr>
-                            <tr>
-                                <td valign="top" id="templateFooter"><table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
-    <tbody class="mcnTextBlockOuter">
-        <tr>
-            <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-                <!--[if mso]>
-                <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
-                <tr>
-                <![endif]-->
-                
-                <!--[if mso]>
-                <td valign="top" width="600" style="width:600px;">
-                <![endif]-->
-                <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
-                    <tbody><tr>
-                        
-                        <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
-                        
-                            <em>Copyright © 2016 <a href="https://github.com/crlcu" target="_blank">Adrian Crisan</a>, All rights reserved.</em>
-                        </td>
-                    </tr>
-                </tbody></table>
-                <!--[if mso]>
-                </td>
-                <![endif]-->
-                
-                <!--[if mso]>
-                </tr>
-                </table>
-                <![endif]-->
-            </td>
-        </tr>
-    </tbody>
-</table></td>
-                            </tr>
                         </table>
                         <!--[if gte mso 9]>
                         </td>
